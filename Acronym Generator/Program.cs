a2a16ff40bd230b3aab162fe3dc6ee65fa66e1db
komp <komp@DESKTOP-A8RDDA2>
2023-09-11 21:36:19 +0200

    Add project files.

---
+++ Acronym Generator/Program.cs
@@ -0,0 +1,31 @@
+﻿try
+{
+    // Gather input from user
+    Console.WriteLine("Please enter a phrase to convert to an acronym: ");
+    String input = Console.ReadLine();
+
+    // Optional exclusions
+    String exclusions = input.Replace("and", "");
+    String[] wordArr = exclusions.Split(' ');
+    Console.WriteLine(wordArr.Length);
+
+    // Loop through sentence
+    String acronym = "";
+    for (int i = 0; i < wordArr.Length; i++)
+    {
+        if (wordArr[i] != "")
+        {
+            acronym += wordArr[i][0];
+        }
+       
+
+    }
+    acronym = acronym.ToUpper();
+    Console.WriteLine("Your acronym is: " + acronym);
+
+}
+catch (Exception e)
+{
+
+    Console.WriteLine( e.ToString());
+}
